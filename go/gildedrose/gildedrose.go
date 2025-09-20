0fc494d142118d1f81938e7b2a6f596151326621
= <=>
2025-09-20 16:37:45 -0400

    initial refactor

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -1,58 +1,70 @@
 package gildedrose
 
+import (
+	"strings"
+)
+
 type Item struct {
 	Name            string
 	SellIn, Quality int
 }
 
 func UpdateQuality(items []*Item) {
-	for i := 0; i < len(items); i++ {
+	for _, item := range items {
+		var shiftAmt int
 
-		if items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].Quality > 0 {
-				if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-					items[i].Quality = items[i].Quality - 1
-				}
-			}
-		} else {
-			if items[i].Quality < 50 {
-				items[i].Quality = items[i].Quality + 1
-				if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].SellIn < 11 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
-						}
-					}
-					if items[i].SellIn < 6 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
-						}
-					}
-				}
-			}
-		}
+		//Update shift amount based on Name
+		switch item.Name {
+		case "Sulfuras, Hand of Ragnaros":
+			continue
+		case "Aged Brie":
+			shiftAmt = 1
 
-		if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-			items[i].SellIn = items[i].SellIn - 1
-		}
-
-		if items[i].SellIn < 0 {
-			if items[i].Name != "Aged Brie" {
-				if items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].Quality > 0 {
-						if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-							items[i].Quality = items[i].Quality - 1
-						}
+		case "Backstage passes to a TAFKAL80ETC concert":
+			{
+				// Backstage passes gain quality as the concert date approaches and fall to 0 after the concert
+				if item.SellIn > 0 && item.SellIn < 11 {
+					if item.SellIn < 6 {
+						shiftAmt = 3
+					} else {
+						shiftAmt = 2
 					}
 				} else {
-					items[i].Quality = items[i].Quality - items[i].Quality
+					item.Quality = 0
 				}
-			} else {
-				if items[i].Quality < 50 {
-					items[i].Quality = items[i].Quality + 1
+			}
+
+		default:
+			{
+				if item.Quality > 0 {
+					if strings.Contains(item.Name, "Conjured") {
+						//Conjured items degrade in quality twice as fast
+						shiftAmt = -2
+					} else {
+						shiftAmt = -1
+					}
 				}
 			}
 		}
-	}
 
+		if item.SellIn > 0 {
+			item.SellIn--
+		}
+		if item.SellIn == 0 && item.Name == "Backstage passes to a TAFKAL80ETC concert" {
+			item.Quality = 0
+			continue
+		}
+
+		if item.SellIn < 0 {
+			if item.Name != "Aged Brie" && item.Quality > 0 {
+				shiftAmt--
+			}
+		}
+
+		item.Quality += shiftAmt
+
+		if item.Quality > 50 {
+			item.Quality = 50
+		}
+	}
 }
