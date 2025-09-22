2d8a911ea0b8a66d3d2cf86b060dab9a06ce3117
= <=>
2025-09-21 20:05:42 -0400

    refactor

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -10,57 +10,44 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             Item item = items[i];
-            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    continue;
-                }
-            if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+            String itemName = item.name;
 
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
+            if (itemName.equals("Sulfuras, Hand of Ragnaros")) {
+                continue;
             }
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn = item.sellIn - 1;
+            if (isAgedProduct(itemName) && item.quality < 50) {
+                addQuality(item);
+            } else if (item.quality > 0){
+                item.quality -- ;
             }
 
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
+            item.sellIn --;
+
+            if (item.sellIn < 0 && itemName.equals("Backstage passes to a TAFKAL80ETC concert")) 
+            {
+                item.quality = 0;
             }
         }
     }
+
+    public boolean isAgedProduct(String itemName){
+        return (itemName.equals("Aged Brie")
+                    || itemName.equals("Backstage passes to a TAFKAL80ETC concert"));
+    }
+
+    public void addQuality(Item item) {
+        if (item.name.equals("Aged Brie")) {
+
+            item.quality++;
+
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.sellIn < 11 && item.quality < 50)
+                item.quality++;
+
+            if (item.sellIn < 6 && item.quality < 50)
+                item.quality++;
+        }
+    }
+
 }
